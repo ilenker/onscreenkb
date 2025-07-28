0ff71d9150e4c172975c06a21d40a22409fc9e85
ilenker <jpswanepoel.42@gmail.com>
2025-07-28 14:25:31 +0200

    basic keytimer logic added

--- main.py
+++ main.py
@@ -1,19 +1,131 @@
 import numpy as np
 import curses
-from curses import wrapper
+import time
+import sys
 
-width = 5
-height = 5
+width = 11
+height = 4
+#       123456789AB
+row1 = "qwert  yuio"
+row2 = "asdfg  hjkl"
+row3 = "zxcvb  pnm "
+row4 = "    =  =   "
+row = "qwert  yuioasdfg  hjklzxcvb  pnm     =  =   "
+#   keymap = np.zeros((width, height), dtype = int)
 
-screen_buffer = np.zeros((width, height), dtype = int)
+#   for i in range(width):
+#       keymap[i][0] = ord(row1[i])
+#       keymap[i][1] = ord(row2[i])
+#       keymap[i][2] = ord(row3[i])
+#       keymap[i][3] = ord(row4[i])
 
-cursor_x = 0
-cursor_y = 0
+keymap = np.zeros((44), dtype = np.uint8)
+keytimers = np.zeros((44), dtype = np.uint8)
+for i in range(44):
+    keymap[i] = ord(row[i])
+
+def init_curses():
+    # Initialize curses
+    stdscr = curses.initscr()
+    curses.noecho()
+    curses.cbreak()
+    curses.curs_set(1)  # Hide the cursor
+    return stdscr
+ 
+def create_pane(stdscr, height, width, y, x):
+    # Create a new window pane
+    pane = stdscr.subwin(height, width, y, x)
+    pane.box()  # Draw a border around the pane
+    pane.refresh()  # Refresh the pane to display
+    return pane
+
+def disable_blocking(pane):
+    # Disable blocking on input
+    pane.nodelay(True)
 
 def main(stdscr):
-    stdscr.clear()
-    stdscr.addstr(10, 10, "wassup (^=__=^)")
-    stdscr.refresh()
-    stdscr.getch()
 
-wrapper(main)
+    init_curses()
+    stdscr.clear()
+    pane = stdscr.subwin(4, 11, 5, 5)
+    debug_pane = stdscr.subwin(5, 12, 0, 0)
+    debug_pane.box()
+    spin_pane = stdscr.subwin(1, 2, 4, 5)
+    disable_blocking(pane)  # Set pane to non-blocking input
+    disable_blocking(spin_pane)  # Set pane to non-blocking input
+    disable_blocking(debug_pane)  # Set pane to non-blocking input
+    
+    curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_BLACK)
+    curses.init_pair(2, curses.COLOR_BLACK, curses.COLOR_WHITE)
+    plain = curses.color_pair(1) 
+    hilite = curses.color_pair(2)
+    key = "2"
+    stdscr.addstr(9, 5, "wassup (^=__=^)")
+    stdscr.refresh()
+    spinner = ["-", "\\", "|", "/"]
+    i_s = 0
+
+    while True:
+        
+
+        if i_s < 3:
+            i_s += 0.03
+        else: 
+            i_s = 0
+        debug_pane.addstr(0, 0, str(keytimers[0]))
+        debug_pane.refresh()
+        if i_s//10 == 0:
+            spin_pane.addstr(0, 0, spinner[int(i_s)])
+            spin_pane.refresh()
+
+        key = "2"
+
+        try:
+            key = pane.getkey()  # Non-blocking input check
+            for i in range(44):
+                if chr(keymap[i])== key:
+                    keytimers[i] = 10
+        except: key = "2"
+             
+        pane.clear()
+
+        # Run through all of the lines in the pane
+        for i in range(43): 
+            if keytimers[i] > 0:
+                pane.addstr(chr(keymap[i]), hilite)
+            else:
+                pane.addstr(chr(keymap[i]), plain) # Print character, auto wrap with addstr
+
+        for i in range(44):
+            if keytimers[i] > 0:
+                keytimers[i] -= 1
+
+        pane.refresh()
+        time.sleep(0.017)
+curses.wrapper(main)
+
+'''
+    qwert    qwert
+    00000    55555
+1       When a key is pressed, change the corresponding key's value in the kb map
+        to timer value, e.g. 5
+1.1     each loop, reduce all the values in kbmap[i][1] by a fixed amount until they reach 0
+
+2   Print the kb map
+2.1     If the characters timer is 0, print the plain text 
+2.2     else, print the highlighted text
+
+        # Run through all of the lines in the pane
+        for i in range(43): 
+            if key == chr(keymap[i]):
+                if pane.getyx()[1] < 10:      # Check for edge and wrap manually  
+                    newy = pane.getyx()[0]    
+                    newx = pane.getyx()[1] + 1
+                else:
+                    newy = pane.getyx()[0] + 1
+                    newx = 0
+                pane.move(newy, newx)         # Skip the position and don't update pane buffer
+                #pane.addstr(chr(keymap[i]), hilite)
+            else:
+                pane.addstr(chr(keymap[i]), plain) # Print character, auto wrap with addstr
+'''
